b83657d5bd00fbfb86c9e649c71dfc94c82d57d8
bchanclo <benoit.chanclou@capgemini.com>
2018-10-26 09:38:31 +0200

    amélioration des tests api

--- test/api.js
+++ test/api.js
@@ -5,11 +5,32 @@ const server = require('../app');
 
 describe('controllers api', () => {
 
+  describe('api unknown', () => {
+
+    describe('GET /api/1/nawak', () => {
+
+      it('should return an error', done => {
+
+        request(server)
+          .get('/api/1/nawak')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(404)
+          .end((err, res) => {
+            should.not.exist(err);
+            done();
+          });
+      });
+
+    });
+
+  });
+
   describe('api aroundme', () => {
 
     describe('GET /api/1/aroundme', () => {
 
-      it('should return list of poi', done => {
+      it('should return list of poi without pos', done => {
 
         request(server)
           .get('/api/1/aroundme')
@@ -19,6 +40,21 @@ describe('controllers api', () => {
           .end((err, res) => {
             should.not.exist(err);
             res.body.should.be.an.Array();
+            res.body.should.containDeep([{id: "592ff81850a6f0070fd25cf5", name: "Kig"}]);
+            done();
+          });
+      });
+      it('should return list of poi with pos', done => {
+
+        request(server)
+          .get('/api/1/aroundme?pos=48.1364079,1.62038')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end((err, res) => {
+            should.not.exist(err);
+            res.body.should.be.an.Array();
+            res.body.should.containDeep([{id: "4fa53738e4b02e6a552e8d24", name: "Cormainville"}]);
             done();
           });
       });
@@ -41,25 +77,28 @@ describe('controllers api', () => {
           .end((err, res) => {
             should.not.exist(err);
             res.body.should.have.properties('id', 'name', 'likes');
+            res.body.id.should.be.equal("5afc0a1481635b002c430e8b");
             done();
           });
+
       });
 
-      describe('GET /api/1/poi/invalid_id', () => {
+    });
 
-        it('should return error 404', done => {
+    describe('GET /api/1/poi/invalid_id', () => {
 
-          request(server)
-            .get(' /api/1/poi/invalid_id')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(404)
-            .end((err, res) => {
-              should.not.exist(err);
-              res.body.should.have.properties('status', 'message');
-              done();
-            });
-        });
+      it('should return error 404', done => {
+
+        request(server)
+          .get('/api/1/poi/invalid_id')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(404)
+          .end((err, res) => {
+            should.not.exist(err);
+            res.body.should.have.properties('status', 'message');
+            done();
+          });
 
       });
 
